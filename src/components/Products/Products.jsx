d0c6a780a40fbcdef7df441b8026511662a17088
kulikovm11 <maksym.obo@gmail.com>
2024-03-04 15:43:01 +0200

    init

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,8 +1,63 @@
+
 import './Products.css'
+import {ProductItem} from "../ProductItem/ProductItem";
+import {useState} from "react";
+import {useTelegram} from "../../hooks";
+
+const products = [
+    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
+    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
+    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
+    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
+    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
+]
+
+const getTotalPrice = (items) => {
+    return items.reduce((acc, item)=>{
+        return acc += item.price
+    },0)
+}
+
+
+
 const Products = () => {
+
+    const [addedItems,setAddedItems] = useState([])
+    const {tg} = useTelegram()
+    const onAdd = (product) => {
+        const alreadyAdded = addedItems.find(i => i.id === product.id)
+        let newItems = []
+
+        if (alreadyAdded) {
+            newItems = addedItems.filter(i => i.id !== product.id)
+        }else {
+            newItems = [...addedItems, product]
+        }
+
+        setAddedItems(newItems)
+
+        if (newItems.length ===0){
+            tg.MainButton.hide()
+        }else {
+            tg.MainButton.show()
+            tg.MainButton.setParams({
+                text: `Купить ${getTotalPrice(newItems)}`
+            })
+        }
+
+    }
     return (
-        <div>
-            Products
+        <div className={'list'}>
+            {products.map(item => (
+                <ProductItem
+                    product={item}
+                    onAdd={onAdd}
+                    className={'item'}
+                />
+            ))}
         </div>
     );
 };
