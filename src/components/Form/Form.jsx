6b00de0aeb7fe1c8f39e68ea9efcfa72b08a3c5d
kulikovm11 <maksym.obo@gmail.com>
2024-03-04 14:55:13 +0200

    init

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -8,21 +8,21 @@ const Form = () => {
     const [subject, setSubject ] = useState()
     const {tg} = useTelegram()
 
-    const onSendData = useCallback(()=>{
-        const data = {
-            country,
-            street,
-            subject
-        }
-        tg.sendData(JSON.stringify(data))
-    },[country,street,subject])
-
-    useEffect(()=>{
-        tg.WebApp.onEvent('mainbtnclck', onSendData)
-        return () => {
-            tg.WebApp.offEvent('mainbtnclck', onSendData)
-        }
-    },[onSendData])
+    // const onSendData = useCallback(()=>{
+    //     const data = {
+    //         country,
+    //         street,
+    //         subject
+    //     }
+    //     tg.sendData(JSON.stringify(data))
+    // },[country,street,subject])
+    //
+    // useEffect(()=>{
+    //     tg.WebApp.onEvent('mainbtnclck', onSendData)
+    //     return () => {
+    //         tg.WebApp.offEvent('mainbtnclck', onSendData)
+    //     }
+    // },[onSendData])
 
     useEffect(() => {
         tg.MainButton.setParams({
